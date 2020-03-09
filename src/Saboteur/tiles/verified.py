587c0e501af6f5bc402690925cf42718680b3a40
Pierre ORHAN <orhan.pierre.france@gmail.com>
2020-03-08 20:36:54 -0400

    removed unused files

--- src/Saboteur/tiles/verified.py
+++
@@ -1,8 +0,0 @@
-import pandas
-import numpy as np
-df=pandas.read_csv("descriptor.csv",sep=" ")
-cardIdx=df.values[:,0]
-cardPower=df.values[:,1]
-cardNumber=np.array(df.values[:,2],dtype=np.uint16)
-print("There is ",np.sum(cardNumber[:19])," paths cards")
-print("There is ",np.sum(cardNumber[19:])," action cards")
